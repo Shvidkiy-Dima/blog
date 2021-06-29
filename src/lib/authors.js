5f0c3dbe7db8f3f11e7abe1c7857e91a4abe8440
d <d.shvidkiy@spider.ru>
2021-06-29 15:19:53 +0300

    lib

---
+++ src/lib/authors.js
@@ -0,0 +1,17 @@
+// import authors from "../meta/authors.yml";
+
+
+
+// const authorMap = generateAuthorMap();
+
+// function generateAuthorMap() {
+//   let result  = {};
+//   for (const author of authors.authors) {
+//     result[author.slug] = author;
+//   }
+//   return result;
+// }
+
+// export function getAuthor(slug) {
+//   return authorMap[slug];
+// }
