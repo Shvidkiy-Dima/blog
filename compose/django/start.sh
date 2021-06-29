7b493d79c11cca8a4cc6d7d7379d2773a14daa74
d <d.shvidkiy@spider.ru>
2021-06-29 17:33:16 +0300

    docker fix

--- compose/django/start.sh
+++ compose/django/start.sh
@@ -11,7 +11,7 @@ try:
     psycopg2.connect(
         dbname="${POSTGRES_DB}",
         user="${POSTGRES_USER}",
-        password="${POSTGRES_PASSWORD}",
+        password="${POSTGRES_PASS}",
         host="${POSTGRES_HOST}",
         port="${POSTGRES_PORT}",
     )
