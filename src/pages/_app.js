9ee3e64173f9474a06aa24551c60fac78f76d127
d <d.shvidkiy@spider.ru>
2021-06-30 15:41:39 +0300

    ym_metrica

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "../styles/global.css";
+import { YMInitializer } from 'react-yandex-metrika';
+import config from '../configs/main.json'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <Component >
+    <YMInitializer accounts={[config.yandex_metrica.id]} options={{webvisor: true}} />
+     {...pageProps} 
+    </Component>
+  )
 }
