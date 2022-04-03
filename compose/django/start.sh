8c33706b9d1d964fa90cb4adbd2a71a551150b36
Dima B <dima@example.com>
2022-04-03 17:37:25 +0300

    fix start django

--- compose/django/start.sh
+++ compose/django/start.sh
@@ -1,35 +1,8 @@
 #!/bin/bash
 
 
-echo "postgres://${POSTGRES_USER}:${POSTGRES_PASS}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
-echo "postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}"
+python /app/check_conn.py --service-name db --port 5432  --ip db
 
-
-postgres_ready() {
-python << END
-import sys
-
-import psycopg2
-
-try:
-    psycopg2.connect(
-        dbname="${POSTGRES_DB}",
-        user="${POSTGRES_USER}",
-        password="${POSTGRES_PASS}",
-        host="${POSTGRES_HOST}",
-        port="${POSTGRES_PORT}",
-    )
-except psycopg2.OperationalError:
-    sys.exit(-1)
-sys.exit(0)
-
-END
-}
-until postgres_ready; do
-  >&2 echo 'Waiting for PostgreSQL to become available...'
-  sleep 1
-done
->&2 echo 'PostgreSQL is available'
 
 python /app/manage.py makemigrations
 python /app/manage.py migrate --fake-initial
